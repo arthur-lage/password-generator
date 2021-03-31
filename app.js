e828f0e54f1ee45141467c111742f09deffd480a
arthur-lage <arthurlage2006@gmail.com>
2021-03-31 20:10:15 -0300

    script added

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-let passwordText = document.getElementById("password") 
 
 function copyPassword(){
-    passwordText.select()
-    passwordText.setSelectRange(0, 99999);
-    document.execCommand("copy")
+    let textToCopy = document.getElementById("password").value
+    navigator.clipboard.writeText(textToCopy)
+        .then(() => { alert(`Copied!`) })
+        .catch((error) => { alert(`Copy failed! ${error}`) })
 }
 
 function generatePassword(){
-
+    let passwordField = document.getElementById("password")
+    let possibleCharacters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklçzxcvbnm1234567890'
+    let passwordLength = 16
+    let newPassword = ''
+    for(i = 0; i < passwordLength; i++){
+        newPassword += possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)]
+    }
+    passwordField.value = newPassword
 }
